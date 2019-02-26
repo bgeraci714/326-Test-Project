06483288483cc7a716882e32e5a3e6863f6e5665
Blake Geraci <bgeraci851@hcc.edu>
2019-02-26 09:51:25 -0500

    adds code to handle todo list behavior

--- todo.js
+++ todo.js
@@ -1,8 +1,108 @@
-let todoList = [];
+const btn = document.getElementById('button');
+const textbox = document.getElementById('textbox');
+const list = document.getElementById('list')
 
-const todoListAdd = (toAdd) => {
-    todoList.push(toAdd);
-}
-addEventListener(onmousedown,MouseEvent){
-    print("this is a test")
-}
+// let todoList = [];
+
+// const todoListAdd = (toAdd) => {
+//     todoList.push(toAdd);
+// }
+
+// addEventListener(onmousedown,MouseEvent){
+//     print("this is a test")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listen only on mousedown 
+// btn.addEventListener('mousedown', () => {
+//     const listItem = document.createElement('li');
+//     listItem.innerText = textbox.value;
+//     list.appendChild(listItem);
+
+//     // clean out old text value
+//     textbox.value = '';
+// });
